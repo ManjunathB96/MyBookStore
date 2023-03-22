8a86988b53c7eb75e79d14fe147a8dd2acd2baf0
ManjunathB96 <bbelagavi6@gmail.com>
2023-03-22 21:40:12 +0530

    [Manjunath S B] Added : remove book from wishlist

--- src/services/wishlist.service.js
+++ src/services/wishlist.service.js
@@ -2,14 +2,17 @@ import Wishlist from '../models/wishlist.model';
 import * as BookService from '../services/book.service';
 
 export const addBookToWishlist = async (userId, bookId) => {
-const book = await BookService.getSingleBook(bookId);
+  const book = await BookService.getSingleBook(bookId);
   if (!book && book.quantity <= 1) {
     throw new Error('Book Not Available  or  Book Out of Stock');
   }
 
-  const wishBook = await Wishlist.findOne({userId: userId,isPurchased: false});
+  const wishBook = await Wishlist.findOne({
+    userId: userId,
+    isPurchased: false
+  });
 
-  console.log("service wishlist details --->",wishBook);
+  console.log('service wishlist details --->', wishBook);
   if (!wishBook) {
     const wishlistBook = await Wishlist.create({
       userId: userId,
@@ -25,15 +28,15 @@ const book = await BookService.getSingleBook(bookId);
     });
     return wishlistBook;
   }
-  let wishlistHasBook = false;
+  let isBookPresent = false;
   for (let idx = 0; idx < wishBook.books.length; idx++) {
-    if(wishBook.books[idx].productID == book._id){
-      wishlistHasBook = true;
+    if (wishBook.books[idx].productID == book._id) {
+      isBookPresent = true;
       break;
     }
   }
   let newWishlist;
-  if(!wishlistHasBook){
+  if (!isBookPresent) {
     newWishlist = Wishlist.updateOne(
       {
         _id: wishBook._id
@@ -54,9 +57,41 @@ const book = await BookService.getSingleBook(bookId);
   return newWishlist;
 };
 
-
 //get all books present in wishlist
-export const getwishlist = async(userId)=>{
-  const books= await Wishlist.find({userId:userId})
-  return books
-}
+export const getwishlist = async (userId) => {
+  const books = await Wishlist.find({ userId: userId });
+  return books;
+};
+
+//remove book from wishlist
+
+export const removeBookFromWishlist = async (userId, bookId) => {
+  const book = await BookService.getSingleBook(bookId);
+  if (!book) {
+    throw new Error('Book is not found');
+  }
+  let list = await Wishlist.findOne({userId:userId});
+  if (!list) {
+    throw new Error('Book is present in wishlist');
+  }
+  let isBookPresent = false;
+  for (let idx = 0; idx < list.books.length; idx++) {
+    if (list.books[idx].productID == book._id) {
+      isBookPresent = true;
+      break;
+    }
+  }
+  let newList;
+  if (isBookPresent) {
+    newList = Wishlist.updateOne({ _id: list._id },
+      {
+        $pull: {
+          books: {
+            productID: book.id
+          }
+        }
+      }
+    );
+  }
+  return newList;
+};
