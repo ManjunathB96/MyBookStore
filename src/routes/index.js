ac94b5dea95434eeef1d8199c502a54aae98ae0c
ManjunathB96 <bbelagavi6@gmail.com>
2023-03-22 23:27:20 +0530

    [Manjunath S B] Added : purchase API

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import userRoute from './user.route';
 import bookRoute from './book.route';
 import cartRoute from './cart.route';
-import wishlistRoute from './wishlist.route';
+//import wishlistRoute from './wishlist.route';
 
 /**
  * Function contains Application routes
@@ -18,7 +18,7 @@ const routes = () => {
 
   router.use('/cart', cartRoute);
 
-  router.use('/wishlist', wishlistRoute);
+ //router.use('/wishlist', wishlistRoute);
 
   return router;
 };
